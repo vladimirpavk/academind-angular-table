cde299fff5dbca083003075b74eac0faab2800ac
Vladimir Pavković <vladimirpavk@gmail.com>
2018-08-13 23:50:41 +0200

    +

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator, MatSort } from '@angular/material';
+import { MatPaginator, MatSort, MatAccordionDisplayMode } from '@angular/material';
 import { DataTableDataSource } from './data-table-datasource';
 
 @Component({
@@ -12,10 +12,21 @@ export class DataTableComponent implements OnInit {
   @ViewChild(MatSort) sort: MatSort;
   dataSource: DataTableDataSource;
 
+  public selectedRow=null;
+
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['id', 'name'];
 
   ngOnInit() {
     this.dataSource = new DataTableDataSource(this.paginator, this.sort);
+  }
+
+  public selectRow(row:any):void{  
+    this.selectedRow=row;
+    console.log(this.selectedRow);
+  }
+
+  public leavedRow(row:any):void{
+    console.log(row);
   }
 }
